9e63f4edc3e865a88cba375cb16273e4e4211c8f
Pulkit Kumar <pulkitkumar95@gamil.com>
2015-02-06 08:03:19 +0530

    Initial Commit

---
+++ Table view challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Table view challenge
+//
+//  Created by Pulkit Kumar on 06/02/15.
+//  Copyright (c) 2015 Pulkit Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Table view challengeTests/Table_view_challengeTests.m
@@ -0,0 +1,40 @@
+//
+//  Table_view_challengeTests.m
+//  Table view challengeTests
+//
+//  Created by Pulkit Kumar on 06/02/15.
+//  Copyright (c) 2015 Pulkit Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Table_view_challengeTests : XCTestCase
+
+@end
+
+@implementation Table_view_challengeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
